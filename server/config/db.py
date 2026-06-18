a60cf5c47bd44b37575d9bc466c458765608dda5
Dharanika <dharanika27112003@gmail.com>
2026-06-18 22:17:42 +0530

    issues while deploying

--- server/config/db.py
+++ server/config/db.py
@@ -1,4 +1,5 @@
 import os
+import certifi
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
@@ -7,8 +8,15 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "test_db")
 
+if not MONGO_URI:
+    raise RuntimeError("MONGO_URI environment variable is not set")
 
-client=MongoClient(MONGO_URI)
+client=MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=10000,
+)
 db=client[DB_NAME]
 
 # User collection
